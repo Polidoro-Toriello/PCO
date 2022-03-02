55ad8470ae31bca491a611ba9467c13bda5c7915
Giovanni Toriello <81221779+giovanni-toriello@users.noreply.github.com>
2022-03-02 13:54:55 +0100

    Creazione oggetto per il prodotto nel pagamento: ProdottoCarrello

---
+++ src/main/java/model/ProdottoCarrello.java
@@ -0,0 +1,31 @@
+package model;
+
+public class ProdottoCarrello {
+    private ProdottoBean product;
+    private int qta;
+    public ProdottoCarrello(ProdottoBean product, int qta) {
+        super();
+        this.product = product;
+        this.qta = qta;
+    }
+    public ProdottoBean getProduct() {
+        return product;
+    }
+    public void setProduct(ProdottoBean product) {
+        this.product = product;
+    }
+    public int getQta() {
+        return qta;
+    }
+    public void setQta(int qta) {
+        this.qta = qta;
+    }
+
+    @Override
+    public String toString() {
+        return "ProdottoCarrello [product=" + product + ", quantità=" + qta + "]";
+    }
+
+
+}
+
