0bed641bfdefd5df68891c4dc5638c7333f84f63
Giovanni Toriello <81221779+giovanni-toriello@users.noreply.github.com>
2022-03-04 08:02:26 +0100

    Aggiunti metodi per aggiungere un articolo, cercare un articolo, calcolare la quantità totale di un articolo nel Carrello: addArticolo, isInCart, checkQuantity

--- src/main/java/model/bean/Carrello.java
+++ src/main/java/model/bean/Carrello.java
@@ -1,6 +1,10 @@
 package model.bean;
 
+import model.dao.ArticoloDao;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Carrello {
@@ -8,13 +12,52 @@ public class Carrello {
     private float totale;
 
     public Carrello() {
-        prodotti=new ArrayList<ArticoloCarrello>();
-        totale=0;
+        prodotti = new ArrayList<ArticoloCarrello>();
+        totale = 0;
     }
 
     public Carrello(List<ArticoloCarrello> prodotti, float totale) {
         this.prodotti = prodotti;
         this.totale = totale;
+    }
+
+    public boolean addArticolo(int id) throws SQLException, ClassNotFoundException {
+        boolean result = false;
+        ArticoloBean articolo = ArticoloDao.doRetrieveById(id);
+        for (ArticoloCarrello articoloCarrello : prodotti) {
+            if (articoloCarrello.getProduct().getCode() == articolo.getCode() && articoloCarrello.getQta() + 1 < articolo.getQtaDisponibile()) {
+                articoloCarrello.setQta(articoloCarrello.getQta() + 1);
+                this.updateTotale();
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public int isInCart(ArticoloBean articolo) {
+        int nProdotti = 0;
+        if (prodotti.isEmpty()) nProdotti = -1;
+        for (int i = 0; i < prodotti.size(); i++) {
+            if (prodotti.get(i).getProduct().getCode() == articolo.getCode()) {
+                nProdotti = i;
+            }
+        }
+        return nProdotti;
+    }
+
+    public boolean checkQuantity(int idProd, int quantita) throws SQLException, ClassNotFoundException {
+        boolean result = false;
+        ArticoloBean articolo = ArticoloDao.doRetrieveById(idProd);
+        int pos = this.isInCart(articolo);
+        int quantitaDesiderata = quantita;
+        if (pos > 0) {
+            quantitaDesiderata += prodotti.get(pos).getQta();
+        }
+
+        if (!(quantitaDesiderata > articolo.getQtaDisponibile())) {
+            result = true;
+        }
+        return result;
     }
 
     public List<ArticoloCarrello> getProdotti() {
@@ -29,7 +72,15 @@ public class Carrello {
         return totale;
     }
 
-    public void setTotale(float totale) {
-        this.totale = totale;
+    public void updateTotale() {
+        float prezzo = 0;
+        for (ArticoloCarrello articoloCarrello : prodotti) {
+            prezzo += articoloCarrello.getProduct().getPrezzo() * articoloCarrello.getQta();
+        }
+        totale = prezzo;
+    }
+
+    public void setTotale(float x) {
+        this.totale = x;
     }
 }
