8c5923a7bd154b2ad5034661b102ed5ea3073c48
rikon311 <polidororiccardo84@gmail.com>
2022-03-05 15:52:19 +0100

    Aggiunta servlet che ridireziona direttamente alla home page + configurazione wweb.xml per eseguire l'applicazione direttamente su tomcat

---
+++ src/main/java/control/HomeServlet.java
@@ -0,0 +1,24 @@
+package control;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getSession().removeAttribute("alertMsg");
+        resp.sendRedirect("view/Home.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+    }
+}
