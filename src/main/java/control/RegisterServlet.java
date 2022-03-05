f1e42f965fc2e60a46bf25cde1dc158252802a7a
Giovanni Toriello <81221779+giovanni-toriello@users.noreply.github.com>
2022-03-05 18:58:41 +0100

    BugFix

--- src/main/java/control/RegisterServlet.java
+++ src/main/java/control/RegisterServlet.java
@@ -4,12 +4,14 @@ import model.bean.UserBean;
 import model.dao.UserDao;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -18,26 +20,31 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req,resp);
         HttpSession session = req.getSession();
         UserBean newUser = new UserBean();
-        try {
-            newUser.setNome(req.getParameter("Nome"));
-            newUser.setCognome(req.getParameter("Cognome"));
-            newUser.setUsername(req.getParameter("Username"));
-            newUser.setEmail("Email");
-            newUser.setPassword("Password");
-            if (UserDao.doInsertUser(newUser)) {
-                newUser.setValid(true);
-                session.setAttribute("utente", newUser);
-                resp.sendRedirect("view/Home.jsp");
-            }else{
+        if (session.getAttribute("utente")!=null && session.getAttribute("manager")!=null) {
+            try {
+                newUser.setNome(req.getParameter("Nome"));
+                newUser.setCognome(req.getParameter("Cognome"));
+                newUser.setUsername(req.getParameter("Username"));
+                newUser.setEmail(req.getParameter("Email"));
+                newUser.setPassword(req.getParameter("Password"));
+                if (UserDao.doInsertUser(newUser)) {
+                    newUser.setValid(true);
+                    session.setAttribute("utente", newUser);
+                    resp.sendRedirect("view/Home.jsp");
+                } else if (!UserDao.doInsertUser(newUser)) {
+                    session.setAttribute("alertMsg", "Registrazione Fallita");
+                    resp.sendRedirect("view/Home.jsp");
+                }
+            } catch (Throwable e) {
+                System.out.println("Ciao");
+                e.printStackTrace();
                 session.setAttribute("alertMsg", "Registrazione Fallita");
+                resp.sendRedirect("view/Home.jsp");
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            session.setAttribute("alertMsg", "Registrazione Fallita");
+        }else{
+            session.setAttribute("alertMsg","Utente già registrato");
             resp.sendRedirect("view/Home.jsp");
         }
     }
