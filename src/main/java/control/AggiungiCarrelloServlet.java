22191355bfb89f78d2f7111fc367d894367078e5
rikon311 <polidororiccardo84@gmail.com>
2022-07-18 17:13:19 +0200

    BugFix catalogo

--- src/main/java/control/AggiungiCarrelloServlet.java
+++ src/main/java/control/AggiungiCarrelloServlet.java
@@ -3,7 +3,6 @@ package control;
 import model.bean.ArticoloBean;
 import model.bean.ArticoloCarrello;
 import model.bean.Carrello;
-import model.dao.ArticoloDao;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.sql.SQLException;
 
 @WebServlet("/aggiungicarrello")
 public class AggiungiCarrelloServlet extends HttpServlet {
@@ -23,15 +21,6 @@ public class AggiungiCarrelloServlet extends HttpServlet {
         HttpSession session = req.getSession();
         int qta= Integer.parseInt(req.getParameter("qta"));
         ArticoloBean articolo = (ArticoloBean) session.getAttribute("articolo");
-        if(articolo == null) {
-            try {
-                articolo = ArticoloDao.doRetrieveById(Integer.parseInt(req.getParameter("idArticolo")));
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
         ArticoloCarrello articoloCarrello = new ArticoloCarrello(articolo, qta);
         Carrello c = (Carrello) session.getAttribute("carrello");
         if(c == null) {
@@ -40,7 +29,7 @@ public class AggiungiCarrelloServlet extends HttpServlet {
         }
         c.addArticolo(articoloCarrello);
         session.setAttribute("carrello", c);
-        resp.sendRedirect("view/Home.jsp");
+        resp.sendRedirect("view/Shop.jsp");
 
 
     }
