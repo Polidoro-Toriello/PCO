c14fbd01753c7eb2f03b7a8f38249aa4c67d2ad0
rikon311 <polidororiccardo84@gmail.com>
2022-08-13 08:53:57 +0200

    Aggiunto focus per input

---
+++ src/main/webapp/js/setFocus.js
@@ -0,0 +1,71 @@
+function setFocusRegisterPage(){
+    let input_nome = document.getElementById("Nome")
+    let input_cognome = document.getElementById("Cognome")
+    let input_email = document.getElementById("Email")
+    let input_username = document.getElementById("Username")
+    let input_password = document.getElementById("inputPassword5")
+    input_nome.focus()
+    input_cognome.focus()
+    input_email.focus()
+    input_username.focus()
+    input_password.focus()
+}
+
+
+function setFocusAddMetodoPage(){
+    let input_nome = document.getElementById("fname")
+    let input_cognome = document.getElementById("lname")
+    let input_email = document.getElementById("Email")
+    let input_carta = document.getElementById("card")
+    let input_scadenza = document.getElementById("scadenza")
+    let input_cvv = document.getElementById("cvv")
+    let input_tipo = document.getElementById("tipo")
+    input_nome.focus()
+    input_cognome.focus()
+    input_email.focus()
+    input_carta.focus()
+    input_scadenza.focus()
+    input_cvv.focus()
+    input_tipo.focus()
+}
+
+
+function setFocusAddArticolo() {
+    let input_nome_prodotto = document.getElementById("nome")
+    let input_descrizione = document.getElementById("descrizione")
+    let input_iva = document.getElementById("iva")
+    let input_prezzo = document.getElementById("prezzo")
+    let input_categoria = document.getElementById("categoria")
+    let input_qta = document.getElementById("qta")
+    input_nome_prodotto.focus()
+    input_descrizione.focus()
+    input_iva.focus()
+    input_prezzo.focus()
+    input_categoria.focus()
+    input_qta.focus()
+
+}
+
+
+function setFocusLoginPage() {
+   let input_username = document.getElementById("Username")
+   let input_password = document.getElementById("inputPassword5")
+   input_username.focus()
+   input_password.focus()
+}
+
+
+function setFocusModificaArticoloPage() {
+    let input_nome_prodotto = document.getElementById("nome")
+    let input_descrizione = document.getElementById("descrizione")
+    let input_iva = document.getElementById("iva")
+    let input_prezzo = document.getElementById("prezzo")
+    let input_categoria = document.getElementById("categoria")
+    let input_qta = document.getElementById("qta")
+    input_nome_prodotto.focus()
+    input_descrizione.focus()
+    input_iva.focus()
+    input_prezzo.focus()
+    input_categoria.focus()
+    input_qta.focus()
+}
